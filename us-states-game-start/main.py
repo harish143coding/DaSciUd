870921401ec5c2e0509ea9dc9b8c08a011e65299
harish moturu <harish.moturu@uni-oldenburg.de>
2022-04-08 16:38:28 +0200

    political map states finding game done

--- us-states-game-start/main.py
+++ us-states-game-start/main.py
@@ -13,8 +13,24 @@ def get_mouse_click_coor(x,y):
     print(x,y)
 
 
-answer_state = screen.textinput(title="guess the state", prompt= "what'S the other state name")
-print(answer_state)
+data = pd.read_csv("50_states.csv")
+all_states = data.state.to_list()
+guessed_states = []
+
+
+while len(guessed_states) <= 50:
+    answer_state = screen.textinput(title= f"{len(guessed_states)}/guess the state",
+                                    prompt="what'S the other state name").title()
+    if answer_state in all_states:
+        guessed_states.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        t.goto(int(state_data.x), int(state_data.y))
+        t.write(answer_state)
+    else:
+        pass
 
 
 # coords_df = pd.read_csv("50_states.csv")
